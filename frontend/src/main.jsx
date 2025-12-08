52774a13f1f4454cc3c45ad2e6b07be355227868
Yihe <>
2025-12-08 21:24:21 +0800

    Revert "Merge remote-tracking branch 'origin/GeminiTest' into ChatGPTTest"
    This reverts commit f2d30e929c71396b16a9a2810947ff9f40198c26, reversing
    changes made to 1bf03ca3c133437e34c69a28c387fb0fe3c0c67c.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from '@/store/store';
-import { BrowserRouter } from 'react-router-dom';
-import '@/index.css'
-import { EntryPoint } from '@/EntryPoint.jsx'
-import AuthBootstrapper from './router/AuthBootstrapper';
-import 'tldraw/tldraw.css'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.VITE_SUBURL}>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <AuthBootstrapper />
-          <EntryPoint />
-        </PersistGate>
-      </Provider>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
